051d182dd0402bffb05e657b217ccb383d778792
DashieTM <fabio.lenherr@gmail.com>
2024-09-02 20:03:12 +0200

    Format lua

--- config/opts.nix
+++ config/opts.nix
@@ -141,19 +141,22 @@
                   end'';
       };
     }
-    #{
-    #  desc = "Init Ionide";
-    #  event = [ "BufEnter" ];
-    #  group = "filetypes";
-    #  pattern = "*.fs";
-    #  callback = {
-    #    __raw = # lua
-    #      ''
-    #        function()
-    #        require("ionide").setup{}
-    #        end'';
-    #  };
-    #}
+    {
+      desc = "Init lua";
+      event = [ "BufEnter" ];
+      group = "filetypes";
+      pattern = "*.lua";
+      callback = {
+        __raw = # lua
+          ''
+            function()
+              vim.opt_local.expandtab = true
+              vim.opt_local.softtabstop = 4
+              vim.opt_local.shiftwidth = 4
+              vim.opt_local.formatoptions:append({ c = true, r = true, o = true, q = true }) 
+            end'';
+      };
+    }
     {
       desc = "Init Overseer";
       event = [ "BufEnter" ];
