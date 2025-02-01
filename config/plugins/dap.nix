e563f6f5f2f8f7feb5cd50339d1baff1a26e2e6f
DashieTM <fabio.lenherr@gmail.com>
2025-02-01 11:24:47 +0100

    Update nixvim

--- config/plugins/dap.nix
+++ config/plugins/dap.nix
@@ -1,4 +1,5 @@
-{ pkgs, ... }: {
+{ pkgs, ... }:
+{
   extraPlugins = with pkgs.vimPlugins; [
     nvim-dap-ui
     nvim-dap-virtual-text
@@ -8,51 +9,73 @@
   ];
   extraPackages = with pkgs; [ vscode-extensions.vadimcn.vscode-lldb.adapter ];
   plugins = {
-    dap = {
-      extensions = {
-        dap-python = { enable = true; };
-        dap-ui = {
-          enable = true;
-          floating.mappings = { close = [ "<ESC>" "q" ]; };
-          extraOptions = { floating = { border = "rounded"; }; };
+    dap-python = {
+      enable = true;
+    };
+    dap-ui = {
+      enable = true;
+      settings = {
+        floating = {
+          mappings = {
+            close = [
+              "<ESC>"
+              "q"
+            ];
+          };
+          border = "rounded";
         };
-        dap-virtual-text = { enable = true; };
       };
+    };
+    dap-virtual-text = {
+      enable = true;
+    };
+    dap = {
       adapters = {
         executables = {
-          codelldb = { command = "${pkgs.lldb_17}/bin/lldb-vscode"; };
+          codelldb = {
+            command = "${pkgs.lldb_17}/bin/lldb-vscode";
+          };
           coreclr = {
             command = "${pkgs.netcoredbg}/bin/netcoredbg";
             args = [ "--interpreter=vscode" ];
           };
           java = {
-            command =
-              "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/bin/java-debug-adapter";
+            command = "${pkgs.vscode-extensions.vscjava.vscode-java-debug}/bin/java-debug-adapter";
           };
-          delve = { command = "${pkgs.delve}/bin/delve"; };
+          delve = {
+            command = "${pkgs.delve}/bin/delve";
+          };
         };
       };
       configurations = {
-        java = [{
-          name = "Debug Java";
-          request = "launch";
-          type = "java";
-        }];
-        go = [{
-          name = "Debug Go";
-          request = "launch";
-          type = "delve";
-        }];
-        cpp = [{
-          name = "Debug CPP";
-          request = "launch";
-          type = "codelldb";
-        }];
-        c = [{
-          name = "Debug C";
-          request = "launch";
-          type = "codelldb";
-        }];
+        java = [
+          {
+            name = "Debug Java";
+            request = "launch";
+            type = "java";
+          }
+        ];
+        go = [
+          {
+            name = "Debug Go";
+            request = "launch";
+            type = "delve";
+          }
+        ];
+        cpp = [
+          {
+            name = "Debug CPP";
+            request = "launch";
+            type = "codelldb";
+          }
+        ];
+        c = [
+          {
+            name = "Debug C";
+            request = "launch";
+            type = "codelldb";
+          }
+        ];
         # no file picker possible here?
         # cs = [{
         #   name = "Debug C#";
