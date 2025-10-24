e413f9ecd841d73e2ca6bef4a2fab946c1f7e6fb
DashieTM <fabio.lenherr@gmail.com>
2025-10-25 00:15:53 +0200

    bugfix(lsp): Remove lsputils reliance

--- config/luaFunctions.nix
+++ config/luaFunctions.nix
@@ -5,12 +5,18 @@
     */
     ''
       local function is_angular_project(root_dir)
+        local function exists(name)
+           local f=io.open(name,"r")
+           if f~=nil then io.close(f) return true else return false end
+        end
+        local function join(path, file)
+            return path .. "/" .. file
+        end
         if root_dir == nil then
           return false
         end
-        local util = require("lspconfig.util")
-        local ang = util.path.exists(util.path.join(root_dir, "angular.json"));
-        local angTemp = util.path.exists(util.path.join(root_dir, "angular-template.json"));
+        local ang = exists(join(root_dir, "angular.json"));
+        local angTemp = exists(join(root_dir, "angular-template.json"));
         local isAngular = ang or angTemp
         return isAngular
       end
