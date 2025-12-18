90f7638890ff3f994ecc3fc83510bc0c0d5b9b6a
DashieTM <fabio.lenherr@gmail.com>
2025-12-18 22:42:13 +0100

    chore(nvf): Move to main branch

--- flake.nix
+++ flake.nix
@@ -3,7 +3,7 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     stable.url = "github:NixOs/nixpkgs/nixos-25.05";
-    nvf.url = "github:notashelf/nvf/v0.8";
+    nvf.url = "github:notashelf/nvf";
     flake-parts.url = "github:hercules-ci/flake-parts";
     base16.url = "github:SenchoPens/base16.nix";
     statix.url = "github:oppiliappan/statix";
